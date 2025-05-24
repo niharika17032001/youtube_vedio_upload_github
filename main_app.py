f7cb7da990dff59c725af75704a1a309e47e663b
niharika17032001 <niharika17032001@gmail.com>
2025-05-24 17:42:06 +0530

    Create main.yml

--- main_app.py
+++ main_app.py
@@ -8,14 +8,13 @@ from check_google_refresh_token import check_credentials
 
 def main():
     if check_credentials():
-        # download_folder_from_drive.main()
-        # youtube_upload.main()
-        # upload_folder_to_drive.main()
+        download_folder_from_drive.main()
+        youtube_upload.main()
+        upload_folder_to_drive.main()
         print("\n🎉 All tasks completed successfully!")
     else:
         print("\n⛔ Aborting YouTube check due to Drive credential failure.")
         sys.exit(1)
-        print("\n🎉 All tasks completed successfully!")
 
 
 if __name__ == "__main__":
